20b5ea6c08d4abfd66cb865cb7313c8f49498a09
Dilan <dilan@dilanxd.com>
2022-08-09 00:27:07 -0500

    change tab width

--- prettier/index.js
+++ prettier/index.js
@@ -2,11 +2,11 @@
 
 /** @type { import('@types/prettier').Config } */
 module.exports = {
-    printWidth: 80,
-    tabWidth: 4,
-    useTabs: false,
-    singleQuote: true,
-    trailingComma: 'es5',
-    arrowParens: 'always',
-    endOfLine: 'lf',
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  singleQuote: true,
+  trailingComma: 'es5',
+  arrowParens: 'always',
+  endOfLine: 'lf',
 };
